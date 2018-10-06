9efad3a9bf2da4c88ba4f55a2644b7f291f4d46f
insighty <project@brstrategies.com>
2018-10-06 05:38:08 -0400

    Add files via upload

---
+++ docker_install.bash
@@ -0,0 +1,32 @@
+##Docker install for dcos
+sudo yum upgrade --assumeyes --tolerant
+sudo yum update --assumeyes
+
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install docker-ce
+
+sudo mkdir -p /etc/systemd/system/docker.service.d && sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOF
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd --storage-driver=overlay
+EOF
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+docker pull nginx
+sudo docker run hello-world
