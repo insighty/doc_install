64520fa5c6941e40bd7c7e8c33946796a6a32799
insighty <project@brstrategies.com>
2018-10-06 10:10:25 -0400

    Update docker_install.bash

--- docker_install.bash
+++ docker_install.bash
@@ -1,7 +1,6 @@
 ##Docker install for dcos
 sudo yum upgrade --assumeyes --tolerant
 sudo yum update --assumeyes
-
 sudo yum remove docker \
                   docker-client \
                   docker-client-latest \
@@ -15,13 +14,10 @@ sudo yum remove docker \
 sudo yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2
-
 sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
-
 sudo yum install docker-ce
-
 sudo mkdir -p /etc/systemd/system/docker.service.d && sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOF
 [Service]
 ExecStart=
